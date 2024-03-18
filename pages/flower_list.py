fbf1e4f34f9e907d6ce2862dcea653aabaa856d9
BuildTools <unconfigured@null.spigotmc.org>
2024-03-19 01:21:11 +0900

    거리순 검색을 구체적으로 설명함"

--- pages/flower_list.py
+++ pages/flower_list.py
@@ -14,6 +14,7 @@ naver_client_id = os.getenv("NAVER_CLIENT_ID")
 naver_client_secret = os.getenv("NAVER_CLIENT_SECRET")
 
 st.header('내 주변 꽃가게 검색하기')
+st.text("최대 5개까지 보여주며 리뷰순은 네이버 기준 입니다.")
 
 col1, col2 = st.columns(2)
 
@@ -57,7 +58,7 @@ def search_shops(location, sort):
         st.write("Error Code:", rescode)
 
 with col1:
-    location = st.text_input("검색할 지역을 입력하세요(5개까지 출력됩니다): ")
+    location = st.text_input("검색할 지역을 입력하세요(ㅇㅇ[시/군/구] ㅇㅇ[읍/면/동]): ")
 
 sort_type = st.radio("정렬 방식 선택:", ["정확도 순", "리뷰 순"])
 
